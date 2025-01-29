f96a0e8c1ca890e60337a7ba2b82fb4361ab32dc
Jeremy <jeremy@test-technique.hitech.fr>
2025-01-29 23:12:18 +0100

    Rajout du constructeur pour AppDbContext (était nécessaire pour l’exécution).

--- HitechTestTechnique.Api/AppDbContext.cs
+++ HitechTestTechnique.Api/AppDbContext.cs
@@ -6,5 +6,10 @@ namespace HitechSoftware.TestTechnique.Donnees;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
     public DbSet<Depense> Depenses { get; set; }
 }
